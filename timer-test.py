a12c1cfa5a9262a6b00cec57ca9cceff7c0f01b2
Jeremy Willden <jeremy.willden@gmail.com>
2024-12-23 19:53:05 -0700

    Adjusted font size and window position to be near the lower-right corner of the second display, if it is immediately to the right of the first one, and assuming both are 1920x1080.

--- timer-test.py
+++ timer-test.py
@@ -1,10 +1,16 @@
+# Install TKinter on Ubuntu
+# sudo apt install python3-tk
+
 import tkinter as tk
 import time
 
 def countdown(count):
     hours, rem = divmod(count, 3600)
     mins, secs = divmod(rem, 60)
-    time_str = f"{hours:02}:{mins:02}:{secs:02}"
+    if hours < 1:
+        time_str = f"{mins:02}:{secs:02}"
+    else:
+        time_str = f"{hours:02}:{mins:02}:{secs:02}"
     time_label.config(text=time_str)
 
     if count > 0:
@@ -20,7 +26,7 @@ def start_countdown():
         time_label.config(text="Invalid input")
 
 root = tk.Tk()
-root.geometry("+100+200")
+root.geometry("+3500+950")
 root.attributes('-topmost',True)
 root.overrideredirect(True)
 #root.withdraw()
@@ -28,7 +34,7 @@ root.overrideredirect(True)
 #root.wm_attributes('-alpha',0.3)
 root.title("Countdown Timer")
 
-time_label = tk.Label(root, text="00:00:00", font=("Arial", 24), fg="#E0E0E0", bg="#020202")
+time_label = tk.Label(root, text="00:00:00", font=("Arial", 48), fg="#E0E0E0", bg="#020202")
 time_label.pack()
 
 root.mainloop()
